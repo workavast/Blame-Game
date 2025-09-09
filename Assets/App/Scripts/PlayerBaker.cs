049f40e1d134b56375f38ca7a252d25d788404f7
Rodion <90834653+workavast@users.noreply.github.com>
2025-09-09 12:49:25 +0300

    create basic movement

---
+++ Assets/App/Scripts/PlayerBaker.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace App.Scripts
+{
+    public struct PlayerTag : IComponentData { }
+
+    public class PlayerBaker : MonoBehaviour
+    {
+        private class Baker : Baker<PlayerBaker>
+        {
+            public override void Bake(PlayerBaker authoring)
+            {
+                var entity = GetEntity(TransformUsageFlags.Dynamic);
+                AddComponent(entity, new PlayerTag());
+            }
+        }
+    }
+    
+    public partial class PlayerInputSystem : SystemBase
+    {
+        private InputSystem_Actions _input;
+        
+        protected override void OnCreate()
+        {
+            _input = new InputSystem_Actions();
+            _input.Enable();
+        }
+
+        protected override void OnUpdate()
+        {
+            var moveDirectionInput = (float2)_input.Player.Move.ReadValue<Vector2>();
+            foreach (var moveDirection in SystemAPI.Query<RefRW<MoveDirection>>().WithAll<PlayerTag>())
+            {
+                moveDirection.ValueRW.Value = moveDirectionInput;
+            }
+        }
+    }
+}
