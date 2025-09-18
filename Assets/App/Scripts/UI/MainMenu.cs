beeec813da88ea36dc6fb943877fef6b50a021f2
Rodion <90834653+workavast@users.noreply.github.com>
2025-09-18 12:08:08 +0300

    add basic menu ui

---
+++ Assets/App/Scripts/UI/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace App.UI
+{
+    public class MainMenu : MonoBehaviour
+    {
+        [SerializeField] private Button startGameBtn;
+        [SerializeField] private Button quitBtn;
+        [SerializeField] private int gameplaySceneIndex;
+
+        private void Awake()
+        {
+            startGameBtn.onClick.AddListener(StartGame);
+            quitBtn.onClick.AddListener(Quit);
+        }
+
+        private void StartGame()
+        {
+            SceneManager.LoadScene(gameplaySceneIndex);
+        }
+
+        private void Quit()
+        {
+            Application.Quit();
+        }
+    }
+}
