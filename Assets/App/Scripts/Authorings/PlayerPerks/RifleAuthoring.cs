bfffa8ef3cc8342615dfdefad2ec41f577a29f15
Rodion <90834653+workavast@users.noreply.github.com>
2025-09-27 21:31:49 +0300

    create rifle perk

---
+++ Assets/App/Scripts/Authorings/PlayerPerks/RifleAuthoring.cs
@@ -0,0 +1,42 @@
+﻿using App.Ecs;
+using App.Ecs.PlayerPerks;
+using Unity.Entities;
+using UnityEngine;
+
+namespace App.Authorings.PlayerPerks
+{
+    public class RifleAuthoring : MonoBehaviour
+    {
+        [SerializeField] private GameObject bulletPrefab;
+        [SerializeField] private float spawnVerticalOffset;
+        [SerializeField] private float damage;
+        [SerializeField] private float moveSpeed;
+        [SerializeField] private float shootPause;
+        [SerializeField] private float distanceReaction;
+        [SerializeField] private int penetration = 1;
+
+        private class Baker : Baker<RifleAuthoring>
+        {
+            public override void Bake(RifleAuthoring authoring)
+            {
+                var entity = GetEntity(TransformUsageFlags.None);
+
+                AddComponent(entity, new RifleTag());
+                AddComponent(entity, new ShootDistanceReaction() { Value = authoring.distanceReaction });
+                AddComponent(entity, new ShootReloadTimer()
+                {
+                    Timer = authoring.shootPause
+                });
+                AddComponent(entity, new BulletInitialData()
+                {
+                    BulletPrefab = GetEntity(authoring.bulletPrefab, TransformUsageFlags.Dynamic),
+                    SpawnVerticalOffset = authoring.spawnVerticalOffset,
+                    Damage = authoring.damage,
+                    MoveSpeed = authoring.moveSpeed,
+                    ShootPause = authoring.shootPause,
+                    Penetration = authoring.penetration
+                });
+            }
+        }
+    }
+}
