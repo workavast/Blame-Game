246748bc3fb369a2b2a25818b28339ecc568b689
Rodion <90834653+workavast@users.noreply.github.com>
2025-10-14 14:22:59 +0300

    improve exp progress view

--- Assets/App/Scripts/ExpManagement/ExpManager.cs
+++ Assets/App/Scripts/ExpManagement/ExpManager.cs
@@ -6,8 +6,10 @@ namespace App.ExpManagement
     {
         public float ExpAmount { get; private set; }
         public float ExpTarget { get; private set; } = 10;
+        public float ExpPrevTarget { get; private set; }
 
         public float FillPercentage => ExpAmount / ExpTarget;
+        public float FillTargetPercentage => (ExpAmount - ExpPrevTarget) / (ExpTarget - ExpPrevTarget);
 
         public bool IsReachExpLimit()
         {
@@ -23,6 +25,7 @@ namespace App.ExpManagement
         
         public void IncreaseExpLimit()
         {
+            ExpPrevTarget = ExpTarget;
             ExpTarget *= 1.35f;
         }
     }
