e7777c10de43fcaa52c40fb6ee65db2d44664bdf
Rodion <90834653+workavast@users.noreply.github.com>
2025-12-10 19:26:28 +0300

    scenes loading little improve

--- Assets/Avastrad/ScenesLoading/DefaultSceneLoader.cs
+++ Assets/Avastrad/ScenesLoading/DefaultSceneLoader.cs
@@ -12,8 +12,6 @@ namespace Avastrad.ScenesLoading
         
         private int _targetSceneIndex = InitialIndex;
 
-        public int PrevTargetSceneIndex { get; private set; } = InitialIndex;
-        
         public event Action OnLoadingStarted;
         public event Action OnLoadingScreenHided;
 
@@ -31,15 +29,14 @@ namespace Avastrad.ScenesLoading
         public void HideLoadScreen(float duration)
             => _loadingScreen.Hide(duration);
 
-        public void LoadScene(int index, float duration, bool skipLoadingScreen = false)
+        public void LoadScene(int index, float duration, bool skipLoadingScene = false)
         {
-            PrevTargetSceneIndex = _targetSceneIndex;
             _targetSceneIndex = index;
             
             OnLoadingStarted?.Invoke();
 
-            if (skipLoadingScreen)
-                SceneManager.LoadScene(_loadingSceneIndex);
+            if (skipLoadingScene)
+                _loadingScreen.Show(duration, () => SceneManager.LoadSceneAsync(_targetSceneIndex));
             else
                 _loadingScreen.Show(duration, () => SceneManager.LoadSceneAsync(_loadingSceneIndex));
         }
--- Assets/Avastrad/ScenesLoading/ISceneLoader.cs
+++ Assets/Avastrad/ScenesLoading/ISceneLoader.cs
@@ -4,14 +4,12 @@ namespace Avastrad.ScenesLoading
 {
     public interface ISceneLoader
     {
-        public int PrevTargetSceneIndex { get; }
-
         public event Action OnLoadingStarted;
         public event Action OnLoadingScreenHided;
 
         public void ShowLoadScreen(float duration, Action onShowedCallback = null);
         public void HideLoadScreen(float duration);
-        public void LoadScene(int index, float duration, bool skipLoadingScreen = false);
+        public void LoadScene(int index, float duration, bool skipLoadingScene = false);
         public void LoadTargetScene();
     }
 }
