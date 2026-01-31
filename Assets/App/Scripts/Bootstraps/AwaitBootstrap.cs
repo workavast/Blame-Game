0824e8211543042a981650d5100c4317124607cc
Rodion <90834653+workavast@users.noreply.github.com>
2026-02-01 02:00:11 +0300

    little clenup

--- Assets/App/Scripts/Bootstraps/AwaitBootstrap.cs
+++ Assets/App/Scripts/Bootstraps/AwaitBootstrap.cs
@@ -1,16 +1,12 @@
 ﻿using System.Threading;
 using System.Threading.Tasks;
-using Avastrad.ScenesLoading;
 using UnityEngine;
-using Zenject;
 
 namespace App.Bootstraps
 {
     public class AwaitBootstrap : Bootstrap
     {
         [SerializeField] private int awaitDelay = 1000;
-
-        [Inject] private readonly ISceneLoader _sceneLoader;
 
         protected override Task SelfInitialization(CancellationToken cancellationToken) 
             => Await(cancellationToken);
