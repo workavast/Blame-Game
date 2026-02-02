93df68a682c1419b18075ef37c54b3d22ccd4c1b
Rodion <90834653+workavast@users.noreply.github.com>
2026-02-02 13:58:25 +0300

    create basic turrets spawning

---
+++ Assets/App/Scripts/Ecs/PlayerPerks/TurretsSpawner/TurretsSpawner.cs
@@ -0,0 +1,73 @@
+﻿using App.Ecs.Attack;
+using App.Ecs.ExistTime;
+using App.Ecs.Player;
+using App.Ecs.Randomisation;
+using App.Ecs.Shooting;
+using App.Ecs.SystemGroups;
+using App.Ecs.Utils;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+namespace App.Ecs.PlayerPerks.TurretsSpawner
+{
+    public struct TurretsSpawnerTag : IComponentData
+    {
+        
+    }
+    
+    public struct TurretsSpawnerData : IComponentData
+    {
+        public Entity TurretPrefab;
+        public int TurretsCount;
+        public float MinDistance;
+        public float MaxDistance;
+        public float Height;
+        public float ExistTime;
+    }
+    
+    [UpdateInGroup(typeof(AfterTransformPausableSimulationGroup))]
+    public partial struct TurretsSpawnerSystem : ISystem
+    {
+        public void OnCreate(ref SystemState state)
+        {
+            state.RequireForUpdate<BeginInitializationEntityCommandBufferSystem.Singleton>();
+            state.RequireForUpdate<PlayerTag>();
+            state.RequireForUpdate<TurretsSpawnerTag>();
+        }
+
+        public void OnUpdate(ref SystemState state)
+        {
+            var playerEntity = SystemAPI.GetSingletonEntity<PlayerTag>();
+            var playerPosition = SystemAPI.GetComponent<LocalTransform>(playerEntity).Position;
+            var globalDamageScale = SystemAPI.GetComponent<AttackDamage>(playerEntity);
+
+            var ecbSystem = SystemAPI.GetSingleton<BeginInitializationEntityCommandBufferSystem.Singleton>();
+            var ecb = ecbSystem.CreateCommandBuffer(state.WorldUnmanaged);
+            
+            foreach (var (data, additionalProjectilesCount, damage, randomHolder, entity) in 
+                     SystemAPI.Query<RefRO<TurretsSpawnerData>, RefRO<AdditionalProjectilesCount>, RefRO<AttackDamage>, 
+                             RefRW<RandomHolder>>()
+                         .WithAll<TurretsSpawnerTag>()
+                         .WithDisabled<AttackCooldown>()
+                         .WithEntityAccess())
+            {
+                SystemAPI.SetComponentEnabled<AttackCooldown>(entity, true);
+
+                var turretsCount = data.ValueRO.TurretsCount + additionalProjectilesCount.ValueRO.Value;
+                var resultDamage = damage.ValueRO.Value * (damage.ValueRO.Scale + globalDamageScale.Scale);
+                for (var i = 0; i < turretsCount; i++)
+                {
+                    var spawnPoint = RandomPosition.GetPointInRadius(playerPosition, data.ValueRO.MinDistance, data.ValueRO.MaxDistance, ref randomHolder.ValueRW.Random);
+                    spawnPoint += new float3(0, data.ValueRO.Height, 0);
+                    
+                    var turretEntity = ecb.Instantiate(data.ValueRO.TurretPrefab);
+                    
+                    ecb.SetComponent(turretEntity, LocalTransform.FromPosition(spawnPoint));
+                    ecb.SetComponent(turretEntity, new AttackDamage() { Value = resultDamage, Scale = 1f });
+                    ecb.SetComponent(turretEntity, new ExistTimer() { Value = data.ValueRO.ExistTime });
+                }
+            }
+        }
+    }
+}
---
+++ Assets/App/Scripts/Ecs/PlayerPerks/TurretsSpawner/TurretsSpawnerAuthoring.cs
@@ -0,0 +1,38 @@
+﻿using App.Ecs.Shooting;
+using App.Ecs.Turrets;
+using Unity.Entities;
+using UnityEngine;
+
+namespace App.Ecs.PlayerPerks.TurretsSpawner
+{
+    public class TurretsSpawnerAuthoring : MonoBehaviour
+    {
+        [SerializeField] private TurretAuthoring turretPrefab;
+        [SerializeField] private int turretsCount;
+        [SerializeField] private float minDistance;
+        [SerializeField] private float maxDistance;
+        [SerializeField] private float height;
+        [SerializeField] private float existTime;
+        
+        private class Baker : Baker<TurretsSpawnerAuthoring>
+        {
+            public override void Bake(TurretsSpawnerAuthoring authoring)
+            {
+                var entity = GetEntity(TransformUsageFlags.None);
+
+                AddComponent(entity, new AdditionalProjectilesCount());
+                
+                AddComponent(entity, new TurretsSpawnerTag());
+                AddComponent(entity, new TurretsSpawnerData()
+                {
+                    TurretPrefab = GetEntity(authoring.turretPrefab, TransformUsageFlags.Dynamic),
+                    TurretsCount = authoring.turretsCount,
+                    MinDistance = authoring.minDistance,
+                    MaxDistance = authoring.maxDistance,
+                    Height = authoring.height,
+                    ExistTime = authoring.existTime
+                });
+            }
+        }
+    }
+}
